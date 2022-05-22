644952dbf248f36b4f6ca13315dd17085a00578f
Wojciech Kabatc <wojciechkabatc@gmail.com>
2022-05-23 00:34:11 +0200

    Wersja v2

--- main.py
+++ main.py
@@ -1,37 +1,8 @@
-class Ksiazka:
-    def __init__(self, tytul, autor):
-        self.tytul = tytul
-        self.autor = autor
-        self.egzemplarze = []
-class Egzemplarz:
-    def __init__(self, rok_wydania, ksiazka):
-        self.rok_wydania = rok_wydania
-        self.ksiazka = ksiazka
-        self.wypozyczony = False
-class Czytelnik:
-    limit = 3
-    def __init__(self, nazwisko):
-        self.nazwisko = nazwisko
-        self.wypozyczone = {}
-    def wypozycz(self, egzemplarz):
-        tytul = egzemplarz.ksiazka.tytul
-        if Czytelnik.limit > len(self.wypozyczone) and tytul not in self.wypozyczone.keys():
-            egzemplarz.wypozyczony = True
-            self.wypozyczone[tytul] = egzemplarz
-            return True
-        else:
-            return False
-    def oddaj(self, tytul):
-        try:
-            self.wypozyczone.pop(tytul).wypozyczony = False
-            return True
-        except KeyError:
-            return False
 class Biblioteka:
     def __init__(self):
         self.__ksiazki = {}
         self.__czytelnicy = {}
-    def dodaj_egzemplarz_ksiazki(self, tytul, autor, rok_wydania):
+    def dodaj(self, tytul, autor, rok_wydania):
         try:
             ksiazka = self.__ksiazki[tytul]
         except KeyError:
@@ -39,11 +10,6 @@ class Biblioteka:
             self.__ksiazki[tytul] = ksiazka
         ksiazka.egzemplarze.append(Egzemplarz(rok_wydania, ksiazka))
         return True
-    def raport_ksiazek(self):
-        lista = []
-        for k in self.__ksiazki.values():
-            lista.append((k.tytul, k.autor, len(k.egzemplarze)))
-        return sorted(lista, key = lambda t: t[0])
     def dostepne_egz(self, tytul):
         try:
             return [ egz for egz in self.__ksiazki[tytul].egzemplarze if not egz.wypozyczony ]
@@ -69,15 +35,49 @@ class Biblioteka:
         b=czytelnik.oddaj(tytul)
         print(b)
 
-biblioteka = Biblioteka()
-lista_czynnosci=[]
-for t in range(int(input())):
-    lista_czynnosci.append(eval(input()))
+class Ksiazka:
+    def __init__(self, tytul, autor):
+        self.tytul = tytul
+        self.autor = autor
+        self.egzemplarze = []
 
-for i in lista_czynnosci:
+class Egzemplarz:
+    def __init__(self, rok_wydania, ksiazka):
+        self.rok_wydania = rok_wydania
+        self.ksiazka = ksiazka
+        self.wypozyczony = False
+
+class Czytelnik:
+    limit = 3
+    def __init__(self, nazwisko):
+        self.nazwisko = nazwisko
+        self.wypozyczone = {}
+    def wypozycz(self, egzemplarz):
+        tytul = egzemplarz.ksiazka.tytul
+        if Czytelnik.limit > len(self.wypozyczone) and tytul not in self.wypozyczone.keys():
+            egzemplarz.wypozyczony = True
+            self.wypozyczone[tytul] = egzemplarz
+            return True
+        else:
+            return False
+    def oddaj(self, tytul):
+        try:
+            self.wypozyczone.pop(tytul).wypozyczony = False
+            return True
+        except KeyError:
+            return False
+
+biblioteka = Biblioteka()
+czynnosci=[]
+for t in range(int(input())):
+    czynnosci.append(eval(input()))
+
+for i in czynnosci:
     if i[0].strip() == "dodaj":
-        print(biblioteka.dodaj_egzemplarz_ksiazki(i[1].strip(), i[2].strip(), i[3]))
-    elif i[0].strip() == "wypozycz":
-        print(biblioteka.wypozycz(i[1].strip(), i[2].strip()))
-    elif i[0].strip() == "oddaj":
-        print(biblioteka.oddaj(i[1].strip(), i[2].strip()))
+        a = biblioteka.dodaj(i[1].strip(), i[2].strip(), i[3])
+    if i[0].strip() == "wypozycz":
+        a = biblioteka.wypozycz(i[1].strip(), i[2].strip())
+    if i[0].strip() == "oddaj":
+        a = biblioteka.oddaj(i[1].strip(), i[2].strip())
+    if (a != None):
+        print(a)
